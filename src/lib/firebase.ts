08e790ec1ca8efb98cf03656aeebaad9b52aacf1
Rishab Chaudhary <rishabchaudhary3010@gmail.com>
2025-09-17 05:52:17 +0000

    can i have the place holder credintials so i can login

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "apiKey": "test-api-key",
-  "authDomain": "test-project-id.firebaseapp.com",
-  "projectId": "test-project-id",
-  "storageBucket": "test-project-id.appspot.com",
-  "messagingSenderId": "123456789012",
-  "appId": "1:123456789012:web:1234567890abcdef123456",
-  "measurementId": "G-MEASUREMENTID"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
